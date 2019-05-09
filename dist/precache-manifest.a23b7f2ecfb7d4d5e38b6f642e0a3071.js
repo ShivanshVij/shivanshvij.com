76f3286aa0d3235cd0d23016b29a6025c57c3df2
ShivanshVij <shivanshvij@outlook.com>
2019-05-08 21:42:25 -0400

    Initial Commit

---
+++ dist/precache-manifest.a23b7f2ecfb7d4d5e38b6f642e0a3071.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2627f1b83c510060fae37ebdfa48a09",
+    "url": "/favicons/apple-touch-icon-57x57-precomposed.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c32f8d5cbd16a9e62491c7c5a124e492",
+    "url": "/favicons/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "a86cfb7cb081d82f0ae3",
+    "url": "/js/app.d6c9650a.js"
+  },
+  {
+    "revision": "13c7d5d6baf417ddc493",
+    "url": "/js/chunk-vendors.fe87fe13.js"
+  },
+  {
+    "revision": "dfa842097ec87c9cd45bc3adb31c036e",
+    "url": "/favicons/apple-touch-icon-180x180-precomposed.png"
+  },
+  {
+    "revision": "a27328651e6a052d8c8d",
+    "url": "/js/about.96ef2973.js"
+  },
+  {
+    "revision": "64b4154cdec86b6dc0a70cb53f4289e7",
+    "url": "/favicons/apple-touch-icon-72x72-precomposed.png"
+  },
+  {
+    "revision": "69cbaa3a9022a2f9fcaa519a360f10c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aea879dd51a4dbae933c77823906409d",
+    "url": "/favicons/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "9c5c30b1081ff3894d097da5315503cc",
+    "url": "/favicons/apple-touch-icon-114x114-precomposed.png"
+  },
+  {
+    "revision": "d72c2a0e17de7d632045d4513934d248",
+    "url": "/favicons/apple-touch-icon-144x144-precomposed.png"
+  },
+  {
+    "revision": "dd86a3d0f0d20b16ac99d2f9a233248b",
+    "url": "/favicons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "c0ec9835670b5d2f5282840eb4438011",
+    "url": "/favicons/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "2b403690b8dafada4291a755a70e4b65",
+    "url": "/favicons/apple-touch-icon-152x152-precomposed.png"
+  },
+  {
+    "revision": "f8aa0d593c861f1fed24f499e885d8e1",
+    "url": "/favicons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "bf010a3153b3e3c5a4a8f34b6057cd1e",
+    "url": "/favicons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "605f20cf48d1e3e6422bb2d4b128050d",
+    "url": "/favicons/site.webmanifest"
+  },
+  {
+    "revision": "f64e5d4c8f3a347f7b729beda8e56754",
+    "url": "/img/logo.f64e5d4c.png"
+  },
+  {
+    "revision": "ec6051e189d7ab53850b128e3b42909e",
+    "url": "/favicons/apple-touch-icon-60x60-precomposed.png"
+  },
+  {
+    "revision": "dfa842097ec87c9cd45bc3adb31c036e",
+    "url": "/favicons/apple-touch-icon-precomposed.png"
+  },
+  {
+    "revision": "b6959b5f9d486e0c0ce00e5da4af8d88",
+    "url": "/favicons/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "5b2e32aadb3f3533352257255381e35d",
+    "url": "/favicons/apple-touch-icon-120x120-precomposed.png"
+  },
+  {
+    "revision": "25a01550f41a4d9a09ca5a3d551a744d",
+    "url": "/favicons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "7db233eec4da313a3c537f10b806881c",
+    "url": "/favicons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "bf010a3153b3e3c5a4a8f34b6057cd1e",
+    "url": "/favicons/apple-touch-icon.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/favicons/browserconfig.xml"
+  },
+  {
+    "revision": "dff9f836bfc51c94d9e3a080fb7f0740",
+    "url": "/favicons/favicon-32x32.png"
+  },
+  {
+    "revision": "1b9b68076732155b89fbdf8c56252963",
+    "url": "/favicons/favicon-16x16.png"
+  },
+  {
+    "revision": "455494328a07e33e7d9494221d1e40c3",
+    "url": "/favicons/apple-touch-icon-76x76-precomposed.png"
+  },
+  {
+    "revision": "73af4ddffaf180117542cf87bfb5faef",
+    "url": "/favicons/mstile-150x150.png"
+  },
+  {
+    "revision": "67682df6118218d5572a55fb368b22fb",
+    "url": "/favicons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "cb8e05ac73c9324616d349907e9a5dda",
+    "url": "/favicons/android-chrome-384x384.png"
+  },
+  {
+    "revision": "0a6d4b4a6233ca558dc87abae8342b7c",
+    "url": "/favicons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "a86cfb7cb081d82f0ae3",
+    "url": "/css/app.4383fdc6.css"
+  }
+];
